a83b97f55dfcec5181db88c1e4a01c6c2c3e028a
Cibele Yamamoto <cibelety@gmail.com>
2019-03-14 08:07:02 -0300

    asdf

--- app/src/main/java/calculadoradegorjetas/fiap/com/br/calculadoradegorjetas/MainActivity.java
+++ app/src/main/java/calculadoradegorjetas/fiap/com/br/calculadoradegorjetas/MainActivity.java
@@ -9,9 +9,17 @@ public class MainActivity extends AppCompatActivity {
     private NumberFormat currencyFormat =
             NumberFormat.getCurrencyInstance();
 
+    private NumberFormat percentFormat =
+            NumberFormat.getPercentInstance();
+
+    private double billAmount;
+
+    private double percent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //inflando a view
         setContentView(R.layout.activity_main);
     }
 
