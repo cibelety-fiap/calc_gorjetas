bb83578c7d78892a8be3b18c89f498db7f23236e
Cibele Yamamoto <cibelety@gmail.com>
2019-03-14 08:00:44 -0300

    asdf

--- app/src/main/java/calculadoradegorjetas/fiap/com/br/calculadoradegorjetas/MainActivity.java
+++ app/src/main/java/calculadoradegorjetas/fiap/com/br/calculadoradegorjetas/MainActivity.java
@@ -1,13 +1,19 @@
 package calculadoradegorjetas.fiap.com.br.calculadoradegorjetas;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NumberFormat currencyFormat =
+            NumberFormat.getCurrencyInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
