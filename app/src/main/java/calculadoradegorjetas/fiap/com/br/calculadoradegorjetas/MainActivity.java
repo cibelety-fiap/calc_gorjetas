d19f652afc81fa55cf043ee09efd1314aa6980fb
Cibele Yamamoto <cibelety@gmail.com>
2019-03-14 08:34:03 -0300

    asdf

--- app/src/main/java/calculadoradegorjetas/fiap/com/br/calculadoradegorjetas/MainActivity.java
+++ app/src/main/java/calculadoradegorjetas/fiap/com/br/calculadoradegorjetas/MainActivity.java
@@ -2,9 +2,25 @@ package calculadoradegorjetas.fiap.com.br.calculadoradegorjetas;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText amountEditText;
+
+    private TextView amountTextView;
+
+    private TextView percentTextView;
+
+    private SeekBar percentSeekBar;
+
+    private TextView tipTextView;
+
+    private TextView totalTextView;
 
     private NumberFormat currencyFormat =
             NumberFormat.getCurrencyInstance();
@@ -21,7 +37,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //inflando a view
         setContentView(R.layout.activity_main);
+
+        amountEditText = findViewById(R.id.amountEditText);
+        amountTextView = findViewById(R.id.amountTextView);
+        percentTextView = findViewById(R.id.percentTextView);
+        percentSeekBar = findViewById(R.id.percentSeekBar);
+        tipTextView = findViewById(R.id.tipTextView);
+        totalTextView = findViewById(R.id.totalTextView);
+
+
     }
-
-
 }
